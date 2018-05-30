96583c03cf32f611f87b9ebe31cff188cf0030f2
KellyMarcilliat <kelly.marcilliat@gmail.com>
2018-05-30 09:24:48 -0600

    Initial Commit

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,8 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_instance_of_activity_exists
+    activity = Activity.new
+    assert_instance_of Activity, Activity
+  end 
 end
